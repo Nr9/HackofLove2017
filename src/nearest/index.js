15ddd18c623ca1237029faf4cf512e033ca49567
Julien Marcil <julien@marcil.com>
2017-03-04 13:00:41 -0500

    impl nearest

--- src/nearest/index.js
+++ src/nearest/index.js
@@ -1,24 +1,37 @@
-import { version } from '../../package.json';
-import { Router } from 'express';
-import { readGeoJson} from '../geo/GeoJsonReader'
-import {Parcometres } from '../geo/Parcometres'
-export default ({ config, db }) => {
-	let api = Router();
+import {version} from '../../package.json';
+import {Router} from 'express';
+import {readGeoJson} from '../geo/GeoJsonReader'
+import {Parcometres} from '../geo/Parcometres'
+export default ({config, db}) => {
+    let api = Router();
 
     let data = readGeoJson('data/parcometre.geojson');
-	let parcometres = new Parcometres(data)
-	// perhaps expose some API metadata at the root
-	api.get('/', (req, res) => {
+    let parcometres = new Parcometres(data);
 
+
+    // perhaps expose some API metadata at the root
+    function getParam(req) {
         let [latitude, longitude] = req.query.location.split(',');
         let radius = req.query.radius;
         let quantity = req.query.quantity;
-        console.dir(location);
-        console.dir(radius);
-        console.dir(quantity);
-		//console.dir(req);
-		res.json(parcometres.findTopNearest({latitude,longitude}, radius, quantity));
-	});
+        return {
+            latitude: parseFloat(latitude),
+            longitude: parseFloat(longitude),
+            radius: parseInt(radius),
+            quantity: parseInt(quantity),
+        };
+    }
 
-	return api;
+    function buildResponse(latitude, longitude, radius, quantity) {
+        let features = parcometres.findTopNearest({latitude, longitude}, radius, quantity);
+        return {name: 'HackOfLove', type: 'FeatureCollection', features};
+    }
+
+    api.get('/', (req, res) => {
+        let {latitude, longitude, radius, quantity} = getParam(req);
+        console.dir({latitude, longitude, radius, quantity});
+        res.json(buildResponse(latitude, longitude, radius, quantity));
+    });
+
+    return api;
 }
